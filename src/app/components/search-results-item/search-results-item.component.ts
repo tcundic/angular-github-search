40362b39827779b5598cc38e77a7e58e2b8993a7
Tomislav Cundic <tcundic@gmail.com>
2019-12-06 19:39:39 +0100

    * Finished search results page design

---
+++ src/app/components/search-results-item/search-results-item.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-search-results-item',
+  templateUrl: './search-results-item.component.html',
+  styleUrls: ['./search-results-item.component.scss']
+})
+export class SearchResultsItemComponent {
+    @Input() item: {};
+}
