01c364e5f67b274e8780a32a8d6fcac969cc37a8
Tomislav Cundic <tcundic@gmail.com>
2019-12-07 11:50:03 +0100

    * Search users works

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-search-bar',
@@ -6,4 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
+  constructor(private router: Router) {}
+
+  @Output() doSearch = new EventEmitter<string>();
+
+  public searchTerm: string;
+
+  public onDoSearch() {
+    this.doSearch.emit(this.searchTerm);
+    this.router.navigate(['/']);
+  }
+
+  public onHomePageVisit() {
+    this.searchTerm = '';
+    this.doSearch.emit(this.searchTerm);
+    this.router.navigate(['/']);
+  }
 }
